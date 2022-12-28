c4d40e067c5b727fee3fc933659714f9bd3270e8
bilalozair <bo569@nyu.edu>
2022-12-28 02:10:34 -0500

    commit tree b734a219a2f88a2087cf37424f78438e551182d5

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function eventHandler(){
+        console.log("Entering password...");
+    }
     return (
-        <div></div>
+        <div>
+        <input onChange = {eventHandler} type="password" id="keypad" name="keypad"/>
+        </div>
     )
 }
 
